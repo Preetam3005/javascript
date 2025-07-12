4f22d8e3093d00b7ea8b2ff3d61bb9c59b89ba94
Preetam Dutta <123827988+Preetam3005@users.noreply.github.com>
2025-07-12 13:57:12 +0000

    2nd day

--- 01_basics/conversio.js
+++ 01_basics/conversio.js
@@ -1,1 +1,18 @@
-let score = 33 
+let score = "33";  
+console.log(typeof score) //string
+let numberinScore = Number(score) // while type conversion ,  datatype starts with capital 
+console.log( typeof numberinScore) //number
+console.log(numberinScore)  //33
+
+
+
+let isLoggedIn = 1 ; 
+let value  = Boolean(isLoggedIn)  ; 
+console.log(value); //true 
+/*
+ if isLoggedIn  == 0  => value  =  false  , 
+ suppose  ,  isLoggedIn = ""  (Empty string) => value =  false
+ but suppose isloggedIn = "Preetam" or any string with length>0  => value = true  ; 
+ */
+
+ 
